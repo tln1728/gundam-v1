1060af536ec72fc08570fed8f1fb082dec11b58c
lam33194 <lamntph33194@fpt.edu.vn>
2025-02-25 17:14:02 +0700

    feat(factory): create factory for User model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -23,12 +24,25 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        // name
+        // email
+        // email_verified_at
+        // password
+        // remember_token
+        // phone
+        // address
+        // avatar
+        // role
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'phone' => fake() -> phoneNumber(),
+            'address' => fake() -> address(),
+            'avatar' => "https://picsum.photos/".fake()->numberBetween(700, 1000),
+            // https://picsum.photos/width/height
         ];
     }
 
@@ -37,7 +51,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
