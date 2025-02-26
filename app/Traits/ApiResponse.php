334720221afa09ac29c15b4c1c0b4dd42c55955f
lam33194 <lamntph33194@fpt.edu.vn>
2025-02-26 21:15:18 +0700

    refactor(api): add comments and improve ApiResponse trait
    - change status field in json response to ("success"/"error") instead of status code
    - change default parameters data = [] instead of null
    - add comments explaining status code, resource usage

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -4,38 +4,52 @@ namespace App\Traits;
 
 trait ApiResponse
 {
-    protected function success($message, $statusCode = 200, $data = null)
+    protected function success($message, $statusCode = 200, $data = [])
     {
         return response()->json([
             'message' => $message,
-            'status'  => $statusCode,
-            'data'    => $data,
-        ], $statusCode);
-    }
-    protected function error($message, $statusCode, $data = null)
-    {
-        return response()->json([
-            'message' => $message,
-            'status'  => $statusCode,
+            'status'  => 'success',
             'data'    => $data,
         ], $statusCode);
     }
 
-    protected function ok($msg, $data = null)
+    /** 
+     * 400 (Bad Request) được sử dụng khi server không thể xử lý yêu cầu do lỗi từ phía client
+     * EG cú pháp sai, dữ liệu không hợp lệ, hoặc thiếu thông tin cần thiết.  
+     */
+    protected function error($message, $statusCode = 400, $data = [])
+    {
+        return response()->json([
+            'message' => $message,
+            'status'  => 'error',
+            'data'    => $data,
+        ], $statusCode);
+    }
+
+    protected function ok($msg, $data = [])
     {
         return $this->success($msg, 200, $data);
     }
 
+    // 201 (Created) tạo mới thành công tài nguyên
+    protected function created($msg, $data = [])
+    {
+        return $this->success($msg, 201, $data);
+    }
+
+    // 404 (Not found) tài nguyên/endpoint ko tồn tại
     protected function not_found($msg)
     {
         return $this->error($msg, 404);
     }
 
+    // 403 (Forbidden) ko có quyền truy cập tài nguyên
     protected function forbidden($msg)
     {
         return $this->error($msg, 403);
     }
 
+    // 401 (Unauthorized): Dùng khi xác thực thất bại (sai email/password).
     protected function unauthorize($msg)
     {
         return $this->error($msg, 401);
