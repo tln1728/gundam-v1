c5c7d72064aa7289485c33bb82ba1958035a390d
lam33194 <lamntph33194@fpt.edu.vn>
2025-02-26 21:29:13 +0700

    fix(migration): add nullable to some fields in user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone',16);
-            $table->string('address');
+            $table->string('phone', 16)->nullable();
+            $table->string('address')->nullable();
             $table->string('avatar')->nullable();
             $table->string('role')->default(User::ROLE_USER);
             $table->rememberToken();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name'  => 'admin',
+            'email' => 'admin@example.com',
+            'role'  => \App\Models\User::ROLE_ADMIN,
+        ]);
+        
+        
+        \App\Models\User::factory(10)->create();
     }
 }
