ce575922192de9c93d29438dc4987844f639dd51
lam33194 <lamntph33194@fpt.edu.vn>
2025-03-09 15:14:04 +0700

    feat(exception): chỉnh thông báo lỗi mặc định của laravel, xóa file thừa

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,8 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -26,5 +28,23 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    
+    public function render($request, Throwable $exception)
+    {
+        if ($request->is('api/*')) {
+            if ($exception instanceof AuthenticationException) {
+                return response()->json([
+                    'message' => 'Bạn cần đăng nhập để thực hiện hành động này',
+                ], 401);
+            }
+
+            if ($exception instanceof NotFoundHttpException) {
+                return response()->json([
+                    'message' => 'Nội dung bạn đang tìm kiếm không tồn tại',
+                ], 404);
+            }
+        }
+        return parent::render($request, $exception);
     }
 }
