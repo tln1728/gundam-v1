a62a0d835a097fcc6b566f8ec2fdf922ecefdbd3
tuanbn <tuanbnph47980@fpt.edu.vn>
2025-03-13 02:53:08 +0700

    Add admin layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,25 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
+import AdminDashBoard from "./pages/admin/AdminDashboard";
+import AdminLayout from "./components/AdminLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/home" element={<Navigate to="/" replace />} />
           <Route path="/product-detail" element={<ProductDetail />} />
         </Route>
+        {/* Admin routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="/admin" element={<AdminDashBoard />} />
+        </Route>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter >
   );
 }
 
