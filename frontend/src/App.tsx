58f049cb0fb678906ed88fe57067389bb88aa19d
txmanh113 <txxmanh@gmail.com>
2025-03-14 00:10:10 +0700

    Thêm giao diện Blog Detail

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 import AdminDashBoard from "./pages/admin/AdminDashboard";
 import AdminLayout from "./components/AdminLayout";
+import BlogDetail from "./pages/BlogDetail";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/home" element={<Navigate to="/" replace />} />
           <Route path="/product-detail" element={<ProductDetail />} />
+          <Route path="/blog-detail" element={<BlogDetail />} />
         </Route>
         {/* Admin routes */}
         <Route path="/admin" element={<AdminLayout />}>
