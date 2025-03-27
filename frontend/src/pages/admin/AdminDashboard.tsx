3faea825ebda862d6c90b8bec6ce0c1ee5410ecf
lam33194 <lamntph33194@fpt.edu.vn>
2025-03-27 19:15:58 +0700

    delete current frontend

--- frontend/src/pages/admin/AdminDashboard.tsx
+++
@@ -1,9 +0,0 @@
-function AdminDashboard() {
-    return (
-        <div style={{ height: "2000px" }}>
-            <h1>Dashboard Content</h1>
-            {/* Your admin page content here */}
-        </div>
-    );
-}
-export default AdminDashboard;
